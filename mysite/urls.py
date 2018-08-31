f2b3d9d1fd621fd8cc5eef76278950eb7ffe7198
unknown <hcghanta@unomaha.edu>
2018-08-31 15:59:21 -0500

    final codes

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
+# from django.conf.urls import url, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path(r'admin/', admin.site.urls),
+    path(r'blog/', include('blog.urls', namespace='blog')),
 ]
